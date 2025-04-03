937264527927e3d11e36dcc1d8dea0aa44cbce81
nakschadiii <105643348+nakschadiii@users.noreply.github.com>
2025-04-03 22:07:44 +0200

    readme

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv/config').config
-const { SUPABASE_DB_API_KEY : key, SUPABASE_DB_URL : url } = process.env;
+const { SUPABASE_DB_TEST_API_KEY : key, SUPABASE_DB_TEST_URL : url } = process.env;
 const supabase = createClient(url, key);
 
 module.exports = supabase;
